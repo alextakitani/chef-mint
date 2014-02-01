a21dbbc63191c84518bed25b991a6a0329e434e1
Manuel Ignacio Franco <maigfrga@gmail.com>
2014-02-01 20:08:26 +0000

    remove productivity tools, adding gen random pass to .bashrc

--- cookbooks/main/recipes/packages.rb
+++ cookbooks/main/recipes/packages.rb
@@ -8,32 +8,32 @@ package 'exuberant-ctags'
 package 'ack-grep'
 package 'python-pip'
 
+#web browsers
+package 'chromium-browser'
+package 'opera'
+
 # Development libraries
+package 'libpq-dev'
 package 'libmysqlclient-dev'
 package 'python2.7-dev'
+package 'python3-dev'
 
 # Web servers
 package 'nginx'
 package 'apache2'
 
-## Web stack
-package 'memcached'
 
 ## SCM
 package 'subversion'
 package 'git'
 package 'mercurial'
 
-## Productivity tools
-package 'empathy'
-package 'irssi'
-package 'clamav'
-
 ## Sysadmin tools
 package 'nmap'
 package 'traceroute'
 package 'curl'
 package 'screen'
+package 'wireshark'
 
 ## Databases
 package 'postgresql'
@@ -43,3 +43,6 @@ package 'mysql-client'
 package 'redis-server'
 package 'mongodb'
 package 'mongodb-server'
+
+##Virtualization
+package 'vagrant'
