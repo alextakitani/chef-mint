6bb6d0fac8104886499819976d0bd3e218da29a8
Alex Takitani <alex.takitani@espm.br>
2014-02-25 09:25:23 -0300

    init knife, librarian

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@
 
 apt-get install -y ruby1.9.1 ruby1.9.1-dev rubygems
 /usr/bin/gem1.9.1 install chef
-sudo chef-solo -c solo.rb -j solo.json
 bundle install --path vendor/bundle --binstubs
 ../bin/librarian-chef install
+sudo chef-solo -c solo.rb -j solo.json
